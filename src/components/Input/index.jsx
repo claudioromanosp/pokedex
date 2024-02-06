c0c5208b7f257f40c0177625c95816f2f4159c3d
¨Claudio <claudio.romano@jolivi.com.br>
2024-02-06 11:51:02 -0300

    criação do componente Input

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,13 @@
+const Input = ({type, placeholder, value, onChange,className}) => {
+    return (
+        <input
+            className={className}
+            type={type}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+      />
+    )
+}
+
+export default Input;
