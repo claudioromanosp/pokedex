a4b8648189797967ddf86ce84a85170dac4ec0d5
¨Claudio <claudio.romano@jolivi.com.br>
2024-02-06 11:52:01 -0300

    input para buscar pokemons específicos

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,28 +1,46 @@
 import { useEffect, useState } from "react";
 import api from "../../services/api";
-import PokemonList from "../../components/PokemonList"
+import PokemonList from "../../components/PokemonList";
+import Input from "../../components/Input";
 
 const Home = () => {
   const [pokemons, setPokemons] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     getPokemon();
   }, []);
-  
 
   const getPokemon = async () => {
     try {
       const response = await api.get(`pokemons.json`);
-        setPokemons(response.data.results);
-        console.log(response.data.results);
+      setPokemons(response.data.results);
+      console.log(response.data.results);
     } catch (error) {
       console.error(error);
     }
-  }
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredPokemons = pokemons.filter(
+    (pokemon) =>
+      pokemon.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      pokemon.national_number.toString().includes(searchTerm)
+  );
+
   return (
     <div>
+      <Input
+        type="text"
+        placeholder="Search by name or national number"
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
       <ul>
-        {pokemons.map((pokemon, index) => (
+        {filteredPokemons.map((pokemon, index) => (
           <li key={index}>
             <PokemonList
               name={pokemon.name}
@@ -30,7 +48,6 @@ const Home = () => {
               type={pokemon.type}
               image={pokemon.sprites.normal}
             />
-
           </li>
         ))}
       </ul>
