77352870e0af8a84ba3f252b0249e0c06227d0ac
¨Claudio <claudio.romano@jolivi.com.br>
2024-02-07 17:15:34 -0300

    ajuste na largura máxima da div

--- src/components/Layout/Container/index.jsx
+++ src/components/Layout/Container/index.jsx
@@ -4,6 +4,7 @@ import { breakpoint } from "../Variables";
 export const ContainerMain = styled.div`
   width: 100%;
   margin: 0 auto;
+  max-width: 1400px;
 `;
 
 function Container({  children }) {
