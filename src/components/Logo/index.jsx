7c53861fa00590643f7ec4439448febc9ef2d4e7
¨Claudio <claudio.romano@jolivi.com.br>
2024-02-07 17:28:38 -0300

    criação do componente Logo

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,11 @@
+import logo from "./pokeball.svg";
+
+function Logo() {
+  return (
+      <div className="logo">
+        <img src={logo} alt="loading"  id="loading" />
+      </div>
+  );
+}
+
+export default Logo;
