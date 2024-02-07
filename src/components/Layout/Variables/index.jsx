bc08fb5374209153fa31c44f1eb237f67954b069
¨Claudio <claudio.romano@jolivi.com.br>
2024-02-07 16:28:05 -0300

    variáreis de cores e breakpoints

---
+++ src/components/Layout/Variables/index.jsx
@@ -0,0 +1,15 @@
+const breakpoint = { 
+  sm: 768,
+  md: 992,
+  lg: 1200
+};
+
+const colors = {
+  font: "#333333",
+  dark_gray:	"#828282",
+  light_gray:	"#F2F2F2",
+  red: "#E2350D",
+  white: "#FFFFFF",
+};
+
+export { breakpoint, colors };
--- src/components/Styles/index.jsx
+++ src/components/Styles/index.jsx
@@ -1,12 +1,6 @@
 import styled from "styled-components";
+import { breakpoint, colors } from "../Layout/Variables";
 
-const colors = {
-  font: "#333333",
-  dark_gray:	"#828282",
-  light_gray:	"#F2F2F2",
-  red: "#E2350D",
-  white: "#FFFFFF",
-};
 
 export const LoadingGif = styled.div`
     width: 200px;
@@ -40,12 +34,6 @@ export const SubtitleH3 = styled.h3`
   color: blue;
 `;
 
-export const Container = styled.div`
-  width: 100%;
-  margin: 0 auto;
-  max-width: 1030px;
-`;
-
 export const ButtonTag = styled.div`
   display: inline-block;
   padding: 5px 20px;
@@ -56,41 +44,6 @@ export const ButtonTag = styled.div`
   border: 1px solid ${(props) => colors.red};
 `;
 
-export const Grid = styled.div`
-  width: 100%;
-  max-width: 1100px;
-  margin: 0 auto;
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-  gap: 20px;
 
-  @media(min-width: 600px){
-    grid-template-columns: repeat(2, 1fr);
-  }
-  @media(min-width: 768px){
-    grid-template-columns: repeat(3, 1fr);
-  }
-  @media(min-width: 992px){
-    grid-template-columns: repeat(4, 1fr);
-  }
-  @media(min-width: 1200px){
-    grid-template-columns: repeat(5, 1fr);
-  }
-`;
-export const GridItem = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 0.5rem;
-`;
-
-export const Nav = styled.div`
-  width: 100%;
-  @media(min-width: 1200px){
-    float: left;
-    max-width: 180px;
-    height: 100vw;
-  }
-`;
 
 
