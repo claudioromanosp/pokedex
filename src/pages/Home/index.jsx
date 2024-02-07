4755cda80722e900b7fd888d3bc8bfdfc3be33bc
¨Claudio <claudio.romano@jolivi.com.br>
2024-02-07 16:27:15 -0300

    aplicando os componentes de Layout

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,14 +3,18 @@ import api from "../../services/api";
 import PokemonList from "../../components/PokemonList";
 import Input from "../../components/Input";
 import Select from "../../components/Select";
-import Image from "../../components/Image";
 import Button from "../../components/Button";
 import Loading from "../../components/Loading";
 import Heart from "../../assets/images/empty-heart.png";
 import HeartFavorite from "../../assets/images/red-heart.png";
-import { Container, Grid, GridItem, Nav, ButtonTag } from "../../components/Styles";
+import {  ButtonTag } from "../../components/Styles";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faToggleOn, faToggleOff } from '@fortawesome/free-solid-svg-icons';
+import ContainerFluid from "../../components/Layout/ContainerFluid";
+import Container from "../../components/Layout/Container";
+import Row from "../../components/Layout/Row";
+import Col from "../../components/Layout/Col";
+
 
 const Home = () => {
   const [pokemons, setPokemons] = useState([]);
@@ -120,6 +124,33 @@ const Home = () => {
   return (
     <div>
       {loading && <Loading />}
+      <section>
+        <header>
+          <div class="container-fluid">
+            <div class="container">
+              <div class="row">
+                <div class="col">logo</div>
+                <div class="col">coluna do meio</div>
+                <div class="col">sair</div>
+              </div>
+            </div>
+          </div>
+        </header>
+      </section>
+
+      <section>
+          <div class="container-fluid">
+            <div class="container">
+              <div class="row">
+                <div class="col">coluna busca</div>
+                <div class="col">select</div>
+                <div class="col">select</div>
+              </div>
+            </div>
+          </div>
+      </section>
+
+     
       <div>
         <Input
           type="text"
@@ -138,24 +169,24 @@ const Home = () => {
         />
       </div>
 
-      <Container>
-        <Nav>
+      <div>
+        
           {showFavoritesOnly ?
              <FontAwesomeIcon 
-            icon={faToggleOn} 
-            style={{ "color": "#E2350D", "fa-secondary-color": "#E2350D" }}
-            size="2x" 
-            onClick={handleClearFavoritesFilter} 
-            />
-            :
-            <FontAwesomeIcon 
-              icon={faToggleOff} 
-              style={{ "color": "#828282", "fa-secondary-color": "#828282" }}  
-              size="2x" 
-              onClick={handleShowFavorites} 
-            />
-       
-          }
+             icon={faToggleOn} 
+             style={{ "color": "#E2350D", "fa-secondary-color": "#E2350D" }}
+             size="2x" 
+             onClick={handleClearFavoritesFilter} 
+             />
+             :
+             <FontAwesomeIcon 
+             icon={faToggleOff} 
+             style={{ "color": "#828282", "fa-secondary-color": "#828282" }}  
+             size="2x" 
+             onClick={handleShowFavorites} 
+             />
+            }
+            </div>
          
           {availableTypes.map((type, index) => (
             <ButtonTag 
@@ -166,27 +197,34 @@ const Home = () => {
               {type}
             </ButtonTag>
           ))}
-        </Nav>
+     
 
-        <Grid>
-          {filteredPokemons.map((pokemon, index) => (
-            <GridItem key={index} className="pokemon-container">
-              <PokemonList
-                name={pokemon.name}
-                national={pokemon.national_number}
-                types={pokemon.type}
-                image={pokemon.sprites.large}
-              />
-              <Button 
-                className={`pokemon-heart ${favorites.includes(pokemon.name) ? "favorite" : ""}`}
-                onClick={() => onHeartClick(pokemon.name)}
-              >
-                <FontAwesomeIcon icon={faHeart} src={favorites.includes(pokemon.name) ? HeartFavorite : Heart} size="2x" />
-              </Button>
-            </GridItem>
-          ))}
-        </Grid>
-      </Container>
+        <ContainerFluid>
+          <Container>
+            <Row>
+              {filteredPokemons.map((pokemon, index) => (
+                <Col key={index}>
+                  <div className="pokemon-container">
+                    <Button 
+                      className={`pokemon-heart ${favorites.includes(pokemon.name) ? "favorite" : ""}`}
+                      onClick={() => onHeartClick(pokemon.name)}
+                      >
+                      <FontAwesomeIcon icon={faHeart} src={favorites.includes(pokemon.name) ? HeartFavorite : Heart} size="2x" />
+                    </Button>
+
+                    <PokemonList
+                      name={pokemon.name}
+                      national={pokemon.national_number}
+                      types={pokemon.type}
+                      image={pokemon.sprites.large}
+                      />
+                    </div>
+                  </Col>
+              ))}
+            </Row>
+          </Container>
+        </ContainerFluid>
+
     </div>
   );
 };
