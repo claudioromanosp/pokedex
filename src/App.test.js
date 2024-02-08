09df9750010490852f567224735eb61a55336c7e
¨Claudio <claudio.romano@jolivi.com.br>
2024-02-08 13:43:10 -0300

    testes unitários do campo de busca e select

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component", () => {
+  test("procura por termo no campo de busca", () => {
+    const { getByPlaceholderText } = render(<App />);
+    const searchInput = getByPlaceholderText(
+      "Pesquisar por nome ou número nacional"
+    );
+    fireEvent.change(searchInput, { target: { value: "charmander" } });
+    expect(searchInput.value).toBe("charmander");
+  });
+
+  test("testando o filtro do select", () => {
+    const { getByLabelText } = render(<App />);
+    const select = getByLabelText("Ordenar por");
+    fireEvent.change(select, { target: { value: "national_number" } });
+    expect(select.value).toBe("national_number");
+  });
 });
