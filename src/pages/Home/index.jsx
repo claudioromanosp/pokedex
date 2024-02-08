dcf4f8ffd1fc52a157cdb48551b83e3c26a7cd34
¨Claudio <claudio.romano@jolivi.com.br>
2024-02-08 13:42:14 -0300

    cofigurando jest para receber import do axios

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -152,6 +152,7 @@ const Home = () => {
               <Col className="col-50" id="col-right">
                 <div className="select-filter">
                   <SubtitleH3>Ordenar por </SubtitleH3>
+                  <label htmlFor="sortOrder">Ordenar por </label>
                   <Select
                     id="sortOrder"
                     value={sortOrder}
@@ -187,14 +188,14 @@ const Home = () => {
               {showFavoritesOnly ?
                 <FontAwesomeIcon 
                 icon={faToggleOn} 
-                style={{ "color": "#E2350D", "fa-secondary-color": "#E2350D" }}
+                style={{ "color": "#E2350D", "--fa-secondary-color": "#E2350D" }}
                 size="2x" 
                 onClick={handleClearFavoritesFilter} 
                 />
                 :
                 <FontAwesomeIcon 
                 icon={faToggleOff} 
-                style={{ "color": "#828282", "fa-secondary-color": "#828282" }}  
+                style={{ "color": "#828282", "--fa-secondary-color": "#828282" }}  
                 size="2x" 
                 onClick={handleShowFavorites} 
                 />
